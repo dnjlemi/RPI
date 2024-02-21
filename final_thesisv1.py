6b083e7aebe4de889c0cdbb4a508a591b543d3a4
KalvinGamboa <68206641+KalvinGamboa@users.noreply.github.com>
2024-02-21 13:54:14 +0800

    Add files via upload

---
+++ final_thesisv1.py
@@ -0,0 +1,231 @@
+import tkinter as tk
+from tkinter import Label, StringVar, font, Button
+import vosk
+import pyaudio
+import time
+import threading
+import numpy as np
+import pyaudio
+from keras_yamnet import params
+from keras_yamnet.yamnet import YAMNet, class_names
+from keras_yamnet.preprocessing import preprocess_input
+#import RPi.GPIO as GPIO
+
+# Set the logging level to suppress Vosk logs
+#vosk.SetLogLevel(-1)
+
+# Initialize GPIO
+#GPIO.setmode(GPIO.BOARD)
+#GPIO.setup(31, GPIO.OUT)
+
+# Function to control vibration
+def vibrate(duration):
+    #GPIO.output(31, True)  # Turn on
+    time.sleep(duration)   # Vibrate for specified duration
+    #GPIO.output(31, False)  # Turn off
+
+# English model
+#model_en = vosk.Model("/home/hp/Downloads/vosk-model-small-en-us-0.15")
+#model_en = vosk.Model(r"C:\Users\hp\Downloads\Thesis Prototype\vosk-sr\vosk-model-small-en-us-0.15")
+#recognizer_en = vosk.KaldiRecognizer(model_en, 16000)
+
+# Filipino model
+#model_ph = vosk.Model("/home/hp/Downloads/vosk-model-tl-ph-generic-0.6")
+#model_ph = vosk.Model(r"C:\Users\hp\Downloads\Thesis Prototype\vosk-sr\vosk-model-tl-ph-generic-0.6")
+#recognizer_ph = vosk.KaldiRecognizer(model_ph, 16000)
+
+#################### MODEL #####################
+model = YAMNet(weights='keras_yamnet/yamnet.h5')
+yamnet_classes = class_names('keras_yamnet/yamnet_class_map.csv')
+plt_classes = [389, 11, 390, 349]  # Alarm Clock, Screaming, Siren, Doorbell
+
+mic = pyaudio.PyAudio()
+stream = mic.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8192)
+stream.start_stream()
+
+WIN_SIZE_SEC = 0.975
+RATE = params.SAMPLE_RATE
+CHUNK = int(WIN_SIZE_SEC * RATE)
+
+# Define the target words and their sound-alike equivalents
+target_words = {
+    "fire": ["pyre","far"], #90% Accuracy
+    "sunog": ["tunog", "so no", "nog", "smoke", "surog", "susog"], #70%
+    "huwag": ["wag"], #70%
+    "warning": ["warning", "burning", "warn", "learning"], #60%
+    "magnanakaw": ["muggle","nanakaw", "nakaw", "mag nakaw"], #60%
+    "stop": ["stop"], #80%
+    "tulong": ["too long", "long", "pulong"], #80%
+    #"hey": ["hey", "okay", "a"],
+    # "uy": ["uy", "oil"],
+    "run": ["run", "rum", "ron"], #90%
+    "takbo": ["takbo", "tabok"], #80%
+    # "hold on": ["hold on", "put on", "doon"],
+    "police": ["police", "please"], # 100%
+    "intruder": ["intruder"], # 80%
+    "emergency": ["emergency", "emergent", "urgency", "agency", "emergence"], #70%
+    "trouble": ["trouble", "tuble", "tumor"],
+    #"evacuate": ["evacuate", "acquit", ],
+    "danger": ["danger", "anger"], #80%
+    "iwas": ["iwas", "he was", "iwasan"], #70%
+    "ingat": ["ingat", "hinga"], #90%
+    "wait": ["wait"], # 100%
+    "care": ["care", "ker", "air"], #80%
+    "call": ["call", "cool"], #80%
+    "alert": ["alert"], #70%
+    #20 words ^^
+    "alarm": ["alarma", "alam"], #80%
+    "lindol": ["lindol"], #new
+    "saklolo": ["saklolo"], #new
+    "tabi": ["tabi"], #new
+    "excuse": ["excuse"], #new
+    "atras": ["atras"], #new
+    "abante": ["abante"], #new
+    "alis": ["alis"], #new
+    "tigil": ["tigil"], #new
+    "bilis": ["bilis"], #new
+    # Add more target words and their sound-alike equivalents
+}
+
+
+# GUI
+class App:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Speech Recognition Alert System")
+
+        # Set custom fonts
+        self.label_font = font.Font(family="Helvetica", size=60, weight="bold")
+        self.active_font = font.Font(family="Helvetica", size=60, weight="bold")
+
+        # Set initial label text
+        self.label_text = StringVar()
+        self.label_text.set("ACTIVE")
+
+        # Create and pack labels
+        self.label = Label(root, textvariable=self.label_text, font=self.active_font, fg="black", bg="white",
+                          width=20, height=3)  # Adjust width and height for smaller GUI
+        self.label.pack(padx=20, pady=20)
+
+        # Add Mode Selection Buttons
+        self.speech_to_text_button = Button(root, text="Speech to Text", command=self.speech_to_text_mode,
+                                             font=("Helvetica", 23))
+        self.speech_to_text_button.pack(side=tk.LEFT, padx=10, pady=10)
+
+        self.sound_recognition_button = Button(root, text="Sound Recognition", command=self.sound_recognition_mode,
+                                              font=("Helvetica", 23))
+        self.sound_recognition_button.pack(side=tk.LEFT, padx=10, pady=30)
+
+        # Add Exit button
+        exit_button = Button(root, text="Exit", command=root.destroy, font=("Helvetica", 23))
+        exit_button.pack(side=tk.LEFT, pady=10)
+
+        self.mode = "speech_to_text"  # Default mode
+        self.speech_to_text_button.config(bg="green")  # Set initial color
+        self.sound_recognition_button.config(bg="red")
+
+        self.root.after(100, self.listen_for_input)
+
+    def flash_word(self, detected_words):
+        if detected_words:
+            word = detected_words[0]
+            self.label_text.set(word)
+            self.label.config(fg="white" if word in target_words else "yellow", bg="red")
+            self.root.update()
+
+            # Use after method to schedule the update after a delay
+            self.root.after(3000, self.reset_label)
+
+    def reset_label(self):
+        self.label_text.set("ACTIVE")
+        self.label.config(fg="black", bg="white")
+        self.root.update()
+
+    def listen_for_input(self):
+        data = stream.read(8192)  # Increase buffer size
+        if self.mode == "speech_to_text":
+            #self.listen_for_speech()
+            test = 0
+        elif self.mode == "sound_recognition":
+            # Add code for sound recognition here
+            self.listen_for_sound()
+            pass
+
+        self.root.after(100, self.listen_for_input)
+
+    def speech_to_text_mode(self):
+        self.mode = "speech_to_text"
+        self.speech_to_text_button.config(bg="green")
+        self.sound_recognition_button.config(bg="red")
+
+    def sound_recognition_mode(self):
+        self.mode = "sound_recognition"
+        self.speech_to_text_button.config(bg="red")
+        self.sound_recognition_button.config(bg="green")
+
+    """ def listen_for_speech(self):
+        mic = pyaudio.PyAudio()
+        stream = mic.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8192)
+        stream.start_stream()
+
+        while True:
+            data = stream.read(8192)
+
+            if recognizer_en.AcceptWaveform(data) or recognizer_ph.AcceptWaveform(data):
+                text_en = recognizer_en.Result()
+                text_ph = recognizer_ph.Result()
+                print(f"English: '{text_en[14:-3]}'")
+                print(f"Filipino: '{text_ph[14:-3]}'")
+                detected_words_en = self.find_matching_words(text_en)
+                detected_words_ph = self.find_matching_words(text_ph)
+
+                detected_words = detected_words_en + detected_words_ph  # Combine detected words from both models
+
+                self.flash_word(detected_words)
+            break
+
+        stream.stop_stream()
+        stream.close()
+        mic.terminate()
+    """
+    def find_matching_words(self, text):
+        detected_words = []
+        for target_word, sound_alike_list in target_words.items():
+            for sound_alike in sound_alike_list:
+                if target_word in text or sound_alike in text:
+                    detected_words.append(target_word)
+                    break
+        return detected_words
+
+    def listen_for_sound(self):
+        mic = pyaudio.PyAudio()
+        stream = mic.open(format=pyaudio.paFloat32, channels=1, rate=RATE, input=True, frames_per_buffer=8192)
+        detection_threshold = 0.7
+        data = preprocess_input(np.fromstring(stream.read(CHUNK), dtype=np.float32), RATE)
+        prediction = model.predict(np.expand_dims(data, 0))[0]
+
+        detected_classes = []
+
+        # Check for specific classes and print an alert
+        for idx, class_idx in enumerate(plt_classes):
+            if prediction[class_idx] > detection_threshold:
+                class_name = yamnet_classes[class_idx]
+                probability = prediction[class_idx]
+                detected_classes.append(class_name)
+
+        self.flash_word(detected_classes)
+
+        stream.stop_stream()
+        stream.close()
+        mic.terminate()
+
+# Initialize and run the GUI
+root = tk.Tk()
+root.geometry("640x480")  # Set the size to fit the 3.5-inch LCD Raspberry Pi screen
+root.configure(bg="white")
+
+app = App(root)
+root.mainloop()
+
+# Cleanup GPIO
+# GPIO.cleanup()
